b3ad9f88ac14e64faf0761860eca074fd08eecd9
issa99-lab <issanana99@gmail.com>
2023-05-04 23:44:44 +0300

    Done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// 'use strict';
+'use strict';
 
-// const btnEl = document.getElementById("button")
-// const billInput = document.getElementById("bill").value;
-// const tipInput = document.getElementById("tip").value;
-// let answerSpan = document.getElementById("totalX");
+const btnEl = document.getElementById("button")
+const billInput = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+let answerSpan = document.getElementById("totalX");
 
-// function totalBill() {
-//   const billValue = billInput.value;
-//   const tipValue = tipInput.value;
-//   const totalValue =   Number(billValue * (1 + tipValue / 100));
-//   console.log(totalValue)
-//   answerSpan.innerText = totalValue.toFixed(2);
-// }
+function totalBill() {
+  const billValue = billInput.value;
+  const tipValue = tipInput.value;
+  const totalValue = (tipValue / 100 + 1) * billValue;
+  console.log(totalValue)
+  answerSpan.innerText = totalValue.toFixed(2);
+}
 
-// btnEl.addEventListener('click', totalBill);
+btnEl.addEventListener('click', totalBill);
